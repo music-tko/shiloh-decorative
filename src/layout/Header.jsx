4eb654fe7b0edf7a96b0a99bbddfea2852e9da0f
thaykhoase <thando@touchfoundry.co.za>
2022-08-13 13:47:26 +0200

    Added font and removed link styles

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,10 +6,10 @@ const Header = () => {
         <div className="nav">
             <img src={logo} />
                 <ul className="links">
-                    <li className="a-link">About Us</li>
-                    <li className="a-link">Services</li>
-                    <li className="a-link">Gallery</li>
-                    <li className="a-link">Contact Us</li>
+                    <a href="#"><li className="a-link">About Us</li></a>
+                    <a href="#"><li className="a-link">Services</li></a>
+                    <a href="#"><li className="a-link">Gallery</li></a>
+                    <a href=""><li className="a-link">Contact Us</li></a>
                 </ul>
         </div>
      );
