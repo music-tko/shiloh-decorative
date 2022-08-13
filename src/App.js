cff6d079b48d2de249f39a1c774e0372a6f88488
thaykhoase <thando@touchfoundry.co.za>
2022-08-13 14:58:40 +0200

    hero image set as well as text

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero";
 import Header from "./layout/Header";
 
 
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="hero-content">
+                <div className="hero-text">
+                    <h1>Over 15+ Years Of Experience In The Industry</h1>
+                    <p>Our experience quarantees client satisfication and high end quality products</p>
+                </div>
+            </div>
+            <div className="socials">
+                <div className="insta"></div>
+                <img src="" alt="" />
+                <img src="" alt="" />
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
