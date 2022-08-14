4902bd752e50c9f19d50e5e0fb22f0baa60e6929
thaykhoase <thando@touchfoundry.co.za>
2022-08-14 19:48:51 +0200

    desktop services section completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 import Header from "./layout/Header";
 
 
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <About />
+      <Services />
     </div>
   );
 }
