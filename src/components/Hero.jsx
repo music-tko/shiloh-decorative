8f320ffb5f898a21945c40d3b21af757148a6d00
thaykhoase <thando@touchfoundry.co.za>
2022-08-14 18:48:51 +0200

    desktop version of hero completed without links

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,16 +2,22 @@ const Hero = () => {
     return (
         <div className="hero">
             <div className="hero-content">
+                <div className="white-bg">
                 <div className="hero-text">
-                    <h1>Over 15+ Years Of Experience In The Industry</h1>
+                    <h1>Over 15 Years Of Experience In The Industry</h1>
                     <p>Our experience quarantees client satisfication and high end quality products</p>
                 </div>
+                <div class="cta">
+                    <a href="/gallery" class="explore">Explore Now</a>
+                </div>
+                <div className="socials">
+                    <div className="facebook"></div>
+                    <div className="instagram"></div>
+                    <div className="linkedin"></div>
+                </div>
+                </div>
             </div>
-            <div className="socials">
-                <div className="insta"></div>
-                <img src="" alt="" />
-                <img src="" alt="" />
-            </div>
+           
         </div>
     );
 }
