101a2550e40ee9aada8771bdf86e701e729f83f6
thaykhoase <thando@touchfoundry.co.za>
2022-09-08 17:09:33 +0200

    fixing issues to do with contact and menu

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,10 +1,10 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/SDI.png'
-import { slide as Menu } from 'react-burger-menu'
 
 const Header = () => {
-  
+    const [isNavExpanded, setIsNavExpanded] = useState(false)
+
     return ( 
 
         <div className="nav">
@@ -15,16 +15,36 @@ const Header = () => {
                 <a href="#photos"><li className="a-link">Gallery</li></a>
                 <a href="#contact"><li className="a-link">Contact Us</li></a>
                 </ul>
-            <Menu burgerButtonClassName={ "burger" }>
-            <a href="#about-us"><li className="a-link">About Us</li></a>
-                <a href="#services"><li className="a-link">Services</li></a>
-                <a href="#photos"><li className="a-link">Gallery</li></a>
-                <a href="#contact"><li className="a-link">Contact Us</li></a>
-                {/* <a id="about" className="menu-item" href="/about">About</a>
-                <a id="contact" className="menu-item" href="/contact">Contact</a>
-                <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a> */} 
-            </Menu>
-        </div>
+                {/* Mobile Menu */}
+                <button className="hamburger" onClick={() => {
+                setIsNavExpanded(!isNavExpanded);
+                }}>
+                {/* icon from heroicons.com */}
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="white"
+                >
+                <path
+                    fillRule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                    clipRule="evenodd"
+                />
+                </svg>
+            </button>
+                <div
+                 className={
+                    isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+                  }>
+                        <ul>
+                            <a href="#about-us"><li className="a-link">About Us</li></a>
+                            <a href="#services"><li className="a-link">Services</li></a>
+                            <a href="#photos"><li className="a-link">Gallery</li></a>
+                            <a href="#contact"><li className="a-link">Contact Us</li></a>
+                        </ul>
+                        </div>
+                </div>
      );
      
 }
